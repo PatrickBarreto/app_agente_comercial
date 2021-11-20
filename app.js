f525be729e2911000b1d3b8a07f47368115636ab
Patrick <contato.patrickbarreto@gmail.com>
2021-11-20 14:42:29 -0300

    versão 1.0

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ const input2 = document.querySelector("#valor2")
 const input3 = document.querySelector("#valor3")
 const retorno = document.querySelector("#retorno")
 const btnCalcular = document.querySelector("#btn-calcular")
+const abrir = document.querySelector("#abrir")
+const fechar = document.querySelector("#fechar")
+const opcoes = document.querySelector("#sessao-opcoes")
 
 
 /* Calcula o custo real do cliente*/
 
  function crc () {
-    let num1 = Number(input1.value)
-    let num2 = Number(input2.value)
-    let num3 = Number(input3.value)
+    let valor_venda = Number(input1.value)
+    let valor_recebido = Number(input2.value)
+    let valor_descontado = Number(input3.value)
     let resultado = 0;
     let resto = 0;
 
-    if (num2 > 0 && num3 <=0 ){
-        resto = num1 - num2;
-        resultado = (resto / num1) * 100;
-        retorno.innerHTML= `O custo real é de ${resultado} %`;
+    if (valor_recebido > 0 && valor_descontado <= 0 ){
+        resto = valor_venda - valor_recebido;
+        resultado = (resto / valor_venda) * 100;
+        retorno.innerHTML= `A taxa real é de ${resultado} %`;
     }
-    else if (num3 > 0 && num2 <= 0){
-        resultado = (num3 / num1) * 100;
+    else if (valor_descontado > 0 && valor_recebido <= 0){
+        resultado = (valor_descontado / valor_venda) * 100;
         retorno.innerHTML= `A taxa real é de ${resultado} %`;
     }
     else{
@@ -30,5 +33,17 @@ const btnCalcular = document.querySelector("#btn-calcular")
 
 btnCalcular.addEventListener("click", crc);
 
+////////////////
 
+function abrir_opcoes (e) {
+    let x = e.target
+    opcoes.style.display="flex"
+}
+function fechar_opcoes (e) {
+    let x = e.target
+    opcoes.style.display="none"
+}
 
+   
+abrir.addEventListener("click", abrir_opcoes)
+fechar.addEventListener("click", fechar_opcoes)
